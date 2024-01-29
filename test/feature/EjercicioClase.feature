f908106140dbdaf5e81243a9bd2efdab832fa6f5
KevinCndor <kevin.condor@epn.edu.ec>
2024-01-29 14:54:01 -0500

    Add EjercicioClase.feature

---
+++ test/feature/EjercicioClase.feature
@@ -0,0 +1,13 @@
+# Created by j-ken at 29/1/2024
+  # language: es
+
+Característica: : Calificar trabajo final de verificacion y validacion
+  Como profesor
+  Quiero calificar el trabajo final de mis estudiantes estrictamente
+  Para aprobar a los estudiantes que demuestran el conocimiento de la materia
+
+  Escenario: : Profesor califica personalmente
+    Dado que un alumno ha entregado su trabajo final a tiempo
+    Cuando el profesor asigna una calificacion sobre 10 basado en una rúbrica
+    Entonces apruebo al estudiante si la calificacion es mayor o igual a 7
+    Pero si no cumple este minimo, repruebo al estudiante
---
+++ test/feature/steps/EjercicioClase.py
@@ -0,0 +1,35 @@
+from behave import *
+
+use_step_matcher("re")
+
+
+@step("que un alumno ha entregado su trabajo final a tiempo")
+def step_impl(context):
+    """
+    :type context: behave.runner.Context
+    """
+    raise NotImplementedError(u'STEP: Dado que un alumno ha entregado su trabajo final a tiempo')
+
+
+@step("el profesor asigna una calificacion sobre 10 basado en una rúbrica")
+def step_impl(context):
+    """
+    :type context: behave.runner.Context
+    """
+    raise NotImplementedError(u'STEP: Cuando el profesor asigna una calificacion sobre 10 basado en una rúbrica')
+
+
+@step("apruebo al estudiante si la calificacion es mayor o igual a 7")
+def step_impl(context):
+    """
+    :type context: behave.runner.Context
+    """
+    raise NotImplementedError(u'STEP: Entonces apruebo al estudiante si la calificacion es mayor o igual a 7')
+
+
+@step("si no cumple este minimo, repruebo al estudiante")
+def step_impl(context):
+    """
+    :type context: behave.runner.Context
+    """
+    raise NotImplementedError(u'STEP: Pero si no cumple este minimo, repruebo al estudiante')
